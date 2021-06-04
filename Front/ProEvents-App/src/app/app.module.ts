ae15f05f326f94806eb622ecf6f28c6b23f1e16e
Ivan Longarai <ivanslongarai@gmail.com>
2021-06-04 20:25:36 -0300

    Angular - Interpolation, Directives, Binding

--- Front/ProEvents-App/src/app/app.module.ts
+++ Front/ProEvents-App/src/app/app.module.ts
@@ -3,23 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
 import { SpeakersComponent } from './speakers/speakers.component';
+import { NavComponent } from './nav/nav.component';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent,
     EventsComponent,
-    SpeakersComponent
-  ],
+    SpeakersComponent,
+    NavComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProEvents-App/src/app/events/events.component.ts
+++ Front/ProEvents-App/src/app/events/events.component.ts
@@ -7,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent implements OnInit {
-  public events: any;
+  
+  public events: any = [];
+  public filteredEvents: any = [];
+
+  public imgWidth: number = 150;
+  public imgMargin: number = 2;
+  public showImages: boolean = false;
+  private _filterBy: string = '';
+
+  public get filterBy(): string {
+    return this._filterBy;
+  }
+  
+  public set filterBy(value: string) {
+    this._filterBy = value;
+    this.filteredEvents = (this.filterBy ? this.filterEvents(this.filterBy) : this.events);
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -17,8 +33,21 @@ export class EventsComponent implements OnInit {
 
   getEvents() {
     this.http.get(`https://localhost:5001/api/Events`).subscribe(
-      (response) => (this.events = response),
+      (response) => {
+        this.events = response; 
+        this.filteredEvents = response
+      },
       (error) => console.log(error)
     );
   }
+
+  filterEvents(filterBy : string) : any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.events.filter(
+      (e: { subject: string; local: string }) => 
+        e.subject.toLocaleLowerCase().indexOf(filterBy) !== -1 || 
+        e.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+      );    
+  }
+
 }
